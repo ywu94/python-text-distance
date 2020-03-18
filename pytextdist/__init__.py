b37be10f956f10e0618a2a50d6a677a762402510
Yifan Wu <ywu@lendingclub.com>
2020-03-17 22:19:03 -0700

    Switch back to logging.yaml and make it optional

--- pytextdist/__init__.py
+++ pytextdist/__init__.py
@@ -1,7 +1,6 @@
 import os
 import logging
 import logging.config
-import json
 
 __name__ = "pytextdist"
 __version__ = "0.1.5"
@@ -19,7 +18,7 @@ importlib.reload(vector_similarity)
 
 """
 Set up logging
-| Default logging configuration can be edited in logging.json.
+| Default logging configuration can be edited in logging.yaml
 | You can also bring customized logging modules.
 """
 
@@ -27,15 +26,16 @@ env_key = "LOG_CFG"
 value = os.getenv(env_key, None)
 
 init_file_dir = os.path.dirname(os.path.abspath(__file__))
-logger_config_json_path = value if value else os.path.join(init_file_dir, "logging.json")
+logger_config_yaml_path = value if value else os.path.join(init_file_dir, "logging.yaml")
 logger_default_level = logging.INFO
 
-if os.path.exists(logger_config_json_path):
-	with open(logger_config_json_path, "rb") as f:
-		try:
-			config = json.load(f)
-		except:
-			config = json.loads(f.read().decode('utf-8'))
-	logging.config.dictConfig(config)
-else:
+try:
+	import yaml
+	if os.path.exists(logger_config_yaml_path):
+		with open(logger_config_yaml_path, "rt") as f:
+			config = yaml.safe_load(f.read())
+		logging.config.dictConfig(config)
+	else:
+		logging.basicConfig(level=logger_default_level)
+except:
 	logging.basicConfig(level=logger_default_level)
